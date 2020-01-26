04445b81553d814fc830133bc8bfa16c0a074117
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-26 04:40:00 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,35 @@
-def using_include(array, element)
-
+def using_includ   true,false
+   array.include? "true"
+  #true 
 end
 
-def using_sort(array)
-
+def using_sort  "false"
+  do array.sort "false"
+                 #true
 end
 
-def using_reverse(array)
+def using_reverse "a,b,c,d"
+  a.reverse_inplace!
+  $ puts a 
+  dcba
+  end
 
+def using_first "a,b,c,d"
+  use_the_first
+  puts "a"
+  abcd
 end
 
-def using_first(array)
-
+def using_last "a,b,c,d"
+  using.last "d"
+  dcba
 end
 
-def using_last(array)
-
-end
-
-def using_size(array)
+def using_size
+  a = [18, 22, 33, nil, 5, 6]
+ #declaring array 
+ b = [1, 4, 1, 1, 88, 9]
+ #size_method 
+"lenght (1) method form : #{a.lenght(1)}/n/n "
 
 end
